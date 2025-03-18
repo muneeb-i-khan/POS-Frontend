ce213fbee8592b1f99b5b583a6448fdf7a6bc1a9
Muneeb Illahi <muneebk015@gmail.com>
2025-03-18 15:17:06 +0530

    ADD: Truncate long strings in abc...xyz format

--- src/app/pages/view-order/view-order.component.ts
+++ src/app/pages/view-order/view-order.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit, inject , Input, Output, EventEmitter} from '@angular/core';
+import { Component, OnInit, inject, Input, Output, EventEmitter } from '@angular/core';
 import { RouterLink, ActivatedRoute, Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgFor, NgIf } from '@angular/common';
 import { OrderService } from '../../services/order.service';
 import { Order } from '../../types/order.type';
 import { RouterModule } from '@angular/router';
+
 @Component({
   selector: 'app-view-order',
   standalone: true,
@@ -12,7 +13,6 @@ import { RouterModule } from '@angular/router';
   templateUrl: './view-order.component.html',
   styleUrls: ['./view-order.component.scss']
 })
-
 export class ViewOrderComponent implements OnInit {
   private orderService = inject(OrderService);
   private route = inject(ActivatedRoute);
@@ -22,13 +22,12 @@ export class ViewOrderComponent implements OnInit {
   @Input() pageSize: number = 10;
   @Output() pageChange = new EventEmitter<number>();
 
-
   columns = [
     { header: 'Order ID', field: 'id' },
     { header: 'Order Date', field: 'orderDate' },
     { header: 'Total Amount', field: 'totalAmount' },
-    {header: 'Customer Name', field: 'customerName'},
-    {header: 'Customer Phone', field: 'customerPhone'}
+    { header: 'Customer Name', field: 'customerName' },
+    { header: 'Customer Phone', field: 'customerPhone' }
   ];
 
   data: Order[] = [];
@@ -38,7 +37,6 @@ export class ViewOrderComponent implements OnInit {
   endDate: string = '';
   errorMessage: string = '';
   showError: boolean = false;
-
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -97,14 +95,12 @@ export class ViewOrderComponent implements OnInit {
   filteredData() {
     return this.data
       .filter(order => {
-
         if (this.startDate && this.endDate) {
           const orderDate = new Date(order.orderDate);
           const start = new Date(this.startDate);
           const end = new Date(this.endDate);
           if (orderDate < start || orderDate > end) return false;
         }
-
 
         if (!this.searchQuery) return true;
 
@@ -130,5 +126,13 @@ export class ViewOrderComponent implements OnInit {
 
   onCreate() {
     this.router.navigate(['/app/orders/create']);
+  }
+
+  truncateValue(value: any): string {
+    const str = value?.toString() || '';
+    if (str.length > 16) {
+      return `${str.slice(0, 3)}...${str.slice(-3)}`;
+    }
+    return str;
   }
 }
